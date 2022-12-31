62996c3e2beeeb6dbb27075201d3a9423fc232e1
Dawit Belay <113282163+DawitSamson@users.noreply.github.com>
2022-12-31 21:39:46 +0200

    Facebook Selenium Automation test

--- README.md
+++ README.md
@@ -25,6 +25,28 @@ An Automation test on Facebook Website using Selenium
 >* Locators = is the py file which I used for variable assigned for Xpath in for the atidStore website
 >* time = is a package that enables working with time in Python
 >* PyTest = is a testing framework that allows users to write test codes using Python programming language. 
+>* Assert = lets you test if a condition in your code returns True, if not, the program will raise an AssertionError.
+
+## 🚧 About The project
+>* This project is Build in Python code Functionality test, and it has multiple functions used to make the codes.
+>* I used Excel to write the test cases for manual testing.
+>* In this project I try to check on 3 parts those are listed below:
+>* ............1.Facebook Registration/Signup
+>* ............2.Facebook Login
+>* ............3.Facebook Forget-password 
+>* By using this I made 2 directory files this are:
+>* ............1.Test (In this there are 3 py file)
+>* ............2.BaseTest (in this there is 1 py file)
+>* to run this test I used Chrome open-source web browser to Run my codes using Pytest. 
+
+
+# 🛠️ Project Tools and Packages Used 
+>On this Project I used 
+>- Python Functions
+>- PyCharm
+>- Selenium 
+>- pytest
+>- Microsoft Excel
 
 # 💻 Source
 - Pycharm software
---
+++ Test/Test_Register.py
@@ -0,0 +1,188 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from BaseTest.Locators_Facebook import *
+from time import sleep
+
+
+def test_Facebook_Validate_Registering_with_Valid_information_by_providing_all_the_fields():
+    driver = webdriver.Chrome()
+    driver.get(Facebook_Web_Address)
+    driver.maximize_window()
+    sleep(2)
+    driver.find_element(By.XPATH, CreateNewAccount_Path).click()  # Click Create New Account button
+    sleep(2)
+    signUp_Page = driver.find_element(By.XPATH,SignUp_PagePath).text  # Display Signup page
+    assert 'Sign Up' == signUp_Page
+    firstName_box = driver.find_element(By.NAME,FirstName_ElementPath)
+    firstName_box.send_keys("david")  # entering first name
+    sleep(2)
+    surname_box = driver.find_element(By.NAME,LastName_ElementPath)
+    surname_box.send_keys("belay")  # entering Last name
+    sleep(2)
+    registerEmail_box = driver.find_element(By.NAME,EmailInput_ElementPath)
+    registerEmail_box.send_keys("youremail@****.com")  # entering email address
+    sleep(2)
+    registerReEnterEmail_box = driver.find_element(By.NAME,EmailReEnter_ElementPath)
+    registerReEnterEmail_box.send_keys("youremail@****.com")  # entering Re-enter email address
+    sleep(2)
+    registerPassword_box = driver.find_element(By.NAME,Password_ElementPath)
+    registerPassword_box.send_keys("password1234")  # entering password
+    sleep(2)
+    birthDay_box = driver.find_element(By.XPATH,BirthDay_ElementPath)
+    birthDay_box.send_keys("13")  # entering Birth Day
+    sleep(2)
+    birthMonth_box = driver.find_element(By.XPATH,BirthMonth_ElementPath)
+    birthMonth_box.send_keys("jul")  # entering Birth Month
+    sleep(2)
+    birthYear_box = driver.find_element(By.XPATH,BirthDay_ElementPath)
+    birthYear_box.send_keys("2000")  # entering Birth Year
+    sleep(2)
+    driver.find_element(By.XPATH,GenderSelect_ElementPath).click()  # Select Gender
+    sleep(2)
+    driver.find_element(By.XPATH,SignUp_ButtonPath).click()  # Click Sign Up Button
+    sleep(5)
+    email_conformation_Page = driver.find_element(By.XPATH,Email_ConformationPath).text  # Display Email conformation page
+    assert 'Enter the code from your email' == email_conformation_Page
+    driver.close()
+    enterConformationNumber = driver.find_element(By.NAME, "code")
+    enterConformationNumber.send_keys("54321")  # entering conformation number
+    sleep(2)
+    driver.find_element(By.XPATH,Continue_ButtonPath).click()  # Click Continue Button
+    accountConform_page = driver.find_element(By.NAME,"Account Confirmed").text  # Display Account conformation page
+    assert 'Account Confirmed' == accountConform_page
+    driver.find_element(By.XPATH,Ok_ButtonPath).click()  # Click Ok button
+    Facebook_Home = driver.current_url  # Display Facebook User Home page
+    assert Facebook_Home == Facebook_Web_Address
+    driver.close()
+
+
+def test_Facebook_Validate_Registering_with_Invalid_information_Using_Null_on_all_fields():
+    driver = webdriver.Chrome()
+    driver.get(Facebook_Web_Address)
+    driver.maximize_window()
+    sleep(2)
+    driver.find_element(By.XPATH,SignUp_ButtonPath).click()  # Click Sign Up Button
+    sleep(5)
+    error_Message = driver.find_element(By.XPATH,"/html[1]/body[1]/div[7]/div[1]/div[1]/div[1]").text  # Display Error Message
+    assert '' == error_Message
+
+
+def test_Facebook_Validate_Registering_with_Invalid_Email():
+    driver = webdriver.Chrome()
+    driver.get(Facebook_Web_Address)
+    driver.maximize_window()
+    sleep(2)
+    driver.find_element(By.XPATH, CreateNewAccount_Path).click()  # Click Create New Account button
+    sleep(2)
+    signUp_Page = driver.find_element(By.XPATH,SignUp_PagePath).text  # Display Signup page
+    assert 'Sign Up' == signUp_Page
+    firstName_box = driver.find_element(By.NAME,FirstName_ElementPath)
+    firstName_box.send_keys("david")  # entering first name
+    sleep(2)
+    surname_box = driver.find_element(By.NAME,LastName_ElementPath)
+    surname_box.send_keys("belay")  # entering Last name
+    sleep(2)
+    registerEmail_box = driver.find_element(By.NAME,EmailInput_ElementPath)
+    registerEmail_box.send_keys("davidsamson06@yahoo.com")  # entering email address
+    sleep(2)
+    registerReEnterEmail_box = driver.find_element(By.NAME,EmailReEnter_ElementPath)
+    registerReEnterEmail_box.send_keys("david@yahoo.com")  # entering Re-enter email address
+    sleep(2)
+    registerPassword_box = driver.find_element(By.NAME,Password_ElementPath)
+    registerPassword_box.send_keys("password1234")  # entering password
+    sleep(2)
+    birthDay_box = driver.find_element(By.XPATH,BirthDay_ElementPath)
+    birthDay_box.send_keys("13")  # entering Birth Day
+    sleep(2)
+    birthMonth_box = driver.find_element(By.XPATH,BirthMonth_ElementPath)
+    birthMonth_box.send_keys("jul")  # entering Birth Month
+    sleep(2)
+    birthYear_box = driver.find_element(By.XPATH,BirthDay_ElementPath)
+    birthYear_box.send_keys("2000")  # entering Birth Year
+    sleep(2)
+    driver.find_element(By.XPATH,GenderSelect_ElementPath).click()  # Select Gender
+    sleep(2)
+    driver.find_element(By.XPATH,SignUp_ButtonPath).click()  # Click Sign Up Button
+    sleep(5)
+    error_Message = driver.find_element(By.XPATH,"/html[1]/body[1]/div[7]/div[1]/div[1]/div[1]").text  # Display Error Message
+    assert '' == error_Message
+
+
+def test_Facebook_Validate_Registering_With_Null_Gender():
+    driver = webdriver.Chrome()
+    driver.get(Facebook_Web_Address)
+    driver.maximize_window()
+    sleep(2)
+    driver.find_element(By.XPATH, CreateNewAccount_Path).click()  # Click Create New Account button
+    sleep(2)
+    signUp_Page = driver.find_element(By.XPATH,SignUp_PagePath).text  # Display Signup page
+    assert 'Sign Up' == signUp_Page
+    firstName_box = driver.find_element(By.NAME,FirstName_ElementPath)
+    firstName_box.send_keys("david")  # entering first name
+    sleep(2)
+    surname_box = driver.find_element(By.NAME,LastName_ElementPath)
+    surname_box.send_keys("belay")  # entering Last name
+    sleep(2)
+    registerEmail_box = driver.find_element(By.NAME,EmailInput_ElementPath)
+    registerEmail_box.send_keys("davidsamson06@yahoo.com")  # entering email address
+    sleep(2)
+    registerReEnterEmail_box = driver.find_element(By.NAME,EmailReEnter_ElementPath)
+    registerReEnterEmail_box.send_keys("david@yahoo.com")  # entering Re-enter email address
+    sleep(2)
+    registerPassword_box = driver.find_element(By.NAME,Password_ElementPath)
+    registerPassword_box.send_keys("password1234")  # entering password
+    sleep(2)
+    birthDay_box = driver.find_element(By.XPATH,BirthDay_ElementPath)
+    birthDay_box.send_keys("13")  # entering Birth Day
+    sleep(2)
+    birthMonth_box = driver.find_element(By.XPATH,BirthMonth_ElementPath)
+    birthMonth_box.send_keys("jul")  # enter Birth Month
+    sleep(2)
+    birthYear_box = driver.find_element(By.XPATH,BirthDay_ElementPath)
+    birthYear_box.send_keys("2000")  # entering Birth Year
+    sleep(2)
+    driver.find_element(By.XPATH,SignUp_ButtonPath).click()  # Click Sign Up Button
+    sleep(5)
+    error_Message = driver.find_element(By.XPATH,"/html[1]/body[1]/div[7]/div[1]/div[1]/div[1]").text  # Display Error Message
+    assert '' == error_Message
+
+
+
+def test_Facebook_Validate_Registering_with_Null_FirstName():
+    driver = webdriver.Chrome()
+    driver.get(Facebook_Web_Address)
+    driver.maximize_window()
+    sleep(2)
+    driver.find_element(By.XPATH, CreateNewAccount_Path).click()  # Click Create New Account button
+    sleep(2)
+    signUp_Page = driver.find_element(By.XPATH,SignUp_PagePath).text  # Display Signup page
+    assert 'Sign Up' == signUp_Page
+    surname_box = driver.find_element(By.NAME,LastName_ElementPath)
+    surname_box.send_keys("belay")  # entering Last name
+    sleep(2)
+    registerEmail_box = driver.find_element(By.NAME,EmailInput_ElementPath)
+    registerEmail_box.send_keys("davidsamson06@yahoo.com")  # entering email address
+    sleep(2)
+    registerReEnterEmail_box = driver.find_element(By.NAME,EmailReEnter_ElementPath)
+    registerReEnterEmail_box.send_keys("david@yahoo.com")  # entering Re-enter email address
+    sleep(2)
+    registerPassword_box = driver.find_element(By.NAME,Password_ElementPath)
+    registerPassword_box.send_keys("password1234")  # entering password
+    sleep(2)
+    birthDay_box = driver.find_element(By.XPATH,BirthDay_ElementPath)
+    birthDay_box.send_keys("13")  # entering Birth Day
+    sleep(2)
+    birthMonth_box = driver.find_element(By.XPATH,BirthMonth_ElementPath)
+    birthMonth_box.send_keys("jul")  # entering Birth Month
+    sleep(2)
+    birthYear_box = driver.find_element(By.XPATH,BirthDay_ElementPath)
+    birthYear_box.send_keys("2000")  # entering Birth Year
+    sleep(2)
+    driver.find_element(By.XPATH,GenderSelect_ElementPath).click()  # Select Gender
+    sleep(2)
+    driver.find_element(By.XPATH,SignUp_ButtonPath).click()  # Click Sign Up Button
+    sleep(5)
+    error_Message = driver.find_element(By.XPATH,"/html[1]/body[1]/div[7]/div[1]/div[1]/div[1]").text  # Display Error Message
+    assert '' == error_Message
+
+
